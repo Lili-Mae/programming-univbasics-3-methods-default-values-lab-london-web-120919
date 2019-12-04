de6f1da990c41481fb5f6ce7d591d6364a778a7b
Lili-Mae Edwards <lilimae@hotmail.co.uk>
2019-12-04 16:34:20 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -7,5 +7,5 @@ def meal_choice(protein="meat", veg1 , veg2)
   return "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice("tofu", "broccoli", "macaroni" )
+meal_choice("broccoli", "macaroni",  )
 
